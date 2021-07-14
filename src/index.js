7504636f514023bc884c8c7dab297797a6718bba
Jeison Cabarcas <ancjeison@gmail.com>
2021-07-14 09:53:03 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const API = "https://platzi-avo.vercel.app/api/avo";
+
+window.fetch(API) // Nos conectamos a la API
+    .then((respuesta) => respuesta.json()) // se reciben los datos de la API Y SE CONVIERTEN EN JSON
+    .then(respuestaJson =>{  // convertivos los datos en un array
+        
+    })
