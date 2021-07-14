1e1043dbf8e653bc0c1801f67cf89b11cff4fca9
Jeison Cabarcas <ancjeison@gmail.com>
2021-07-14 10:32:11 -0500

    API conectada mas contenedores

--- src/index.js
+++ src/index.js
@@ -3,19 +3,28 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-const API = "https://platzi-avo.vercel.app/api/avo";
 
-window.fetch(API) // Nos conectamos a la API
+const baseUrl = "https://platzi-avo.vercel.app";
+
+const appNode =document.querySelector("#container")
+
+window.fetch(`${baseUrl}/api/avo`) // Nos conectamos a la API
     .then((respuesta) => respuesta.json()) // se reciben los datos de la API Y SE CONVIERTEN EN JSON
     .then(respuestaJson => {  // convertivos los datos en un array
         const allitems=[];
         respuestaJson.data.forEach((item) => {
             const imagen = document.createElement("img");
+            imagen.src=`${baseUrl}${item.image}`;
+
             const titulo = document.createElement("h2");
+            titulo.textContent=item.name;
+
             const price = document.createElement("div");
+            price.textContent=item.price;
+
             const container = document.createElement("div");
             container.append(imagen,titulo,price); 
             allitems.push(container);
         });
-        document.body.append(...allitems);
+        appNode.append(...allitems);
     })
