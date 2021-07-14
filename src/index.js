892a13d4f4120ef4f90b81a15e42c494c681043c
Jeison Cabarcas <ancjeison@gmail.com>
2021-07-14 10:02:26 -0500

    Creacion de contenedores

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ const API = "https://platzi-avo.vercel.app/api/avo";
 
 window.fetch(API) // Nos conectamos a la API
     .then((respuesta) => respuesta.json()) // se reciben los datos de la API Y SE CONVIERTEN EN JSON
-    .then(respuestaJson =>{  // convertivos los datos en un array
-        
+    .then(respuestaJson => {  // convertivos los datos en un array
+        const allitems=[];
+        respuestaJson.data.forEach((item) => {
+            const imagen = document.createElement("img");
+            const titulo = document.createElement("h2");
+            const price = document.createElement("div");
+            const container = document.createElement("div");
+            container.append(imagen,titulo,price); 
+            allitems.push(container);
+        });
+        document.body.append(...allitems);
     })
